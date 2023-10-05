10c94e1737b20974f3d9d036a1b0f64529bee1f6
siskoweb <sisko4dev@gmail.com>
2023-10-05 12:18:38 +0100

    debugging url assets

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,11 +3,14 @@ filterSelection("all")
 
 function filterSelection(cate) {
     var cards, i;
-    cards = document.getElementsByClassName("card-menu-filter");
+    cards = document.getElementsByClassName("card-menu-filter");// get all cards
     console.log(cards)
-    if (cate == "all") cate = "";
+    if (cate == "all") cate = ""; // if user click on all display all cards
+
     for (i = 0; i < cards.length; i++) {
         removeClass(cards[i], "show");
+        // add show class to card has same category 
+        //find index card useing ondexof 
         if (cards[i].className.indexOf(cate) > -1) addClass(cards[i], "show");
     }
 }
@@ -34,10 +37,12 @@ function removeClass(element, name) {
 }
 
 
-
+// get dev contain all btns
 var btnContainer = document.getElementById("btnMenusFilter");
+// get all btns
 var btns = btnContainer.getElementsByClassName("btnFilter");
 
+// loop throught a btns and remove active class from it
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
         // loop throught all btns to Remove the active class 
